3eeed93991e87c906527dcad817c58b145ccca56
MMA-dev-collab <thethreemma@gmail.com>
2026-01-16 14:50:36 +0200

    Delete migrations directory

--- migrations/add_phone_to_users.sql
+++
@@ -1,5 +0,0 @@
--- Add phone column to users table if it doesn't exist
--- Also add unique constraint for phone number
-
-ALTER TABLE users ADD COLUMN IF NOT EXISTS phone VARCHAR(20);
-CREATE UNIQUE INDEX IF NOT EXISTS idx_users_phone ON users(phone);
